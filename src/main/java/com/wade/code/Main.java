1ea70c9839d4e121e78f88e6d1d82ff37a003c69
cw <729051833@qq.com>
2022-01-26 15:23:18 +0800

    init

---
+++ src/main/java/com/wade/code/Main.java
@@ -0,0 +1,13 @@
+package com.wade.code;
+public class Main {
+    class Solution {
+        public int search(int[] nums, int target) {
+
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
